db29a3e96078ea21075ee6fee4970cffd5cdc202
sinland <sinland@mail.ru>
2013-07-12 16:54:29 +0600

    settings changed

--- snzphoto/settings.py
+++ snzphoto/settings.py
@@ -1,22 +1,20 @@
+import os
+
 # Django settings for snzphoto project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    # ('sinland', 'sinland@mail.ru'),
 )
 
 MANAGERS = ADMINS
-
+DIR = os.path.dirname(__file__)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(DIR, 'snzphotosite.s3db'),
     }
 }
 
